ebe4b2f8d74c7eb1eb3360d003bef630752f6ab5
Cj_dozie <126397480+Chidozie222@users.noreply.github.com>
2024-12-10 16:45:54 +0100

    Fix

--- screens/plans/choosePlanCalender.js
+++ screens/plans/choosePlanCalender.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   View,
@@ -7,8 +7,10 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
+import Checkbox from "expo-checkbox";
 
 const ChoosePlanCalender = () => {
+  const [isChecked, setChecked] = useState(false);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
@@ -17,18 +19,192 @@ const ChoosePlanCalender = () => {
         </View>
         <View style={styles.MainCardContainer}>
           <View style={styles.card}>
-              <Text
-                style={styles.CardText({ fontSize: 24, marginVertical: 6 })}
-              >
-                Pro
-              </Text>
+            <Text style={styles.CardText({ fontSize: 24, marginVertical: 6 })}>
+              Pro
+            </Text>
+            <View style={styles.table}>
+              <View style={styles.tableChecKBoxContainer}>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Jan</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Feb</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Mar</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Apr</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>May</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+              </View>
+              <View style={styles.tableChecKBoxContainer}>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Jun</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Jul</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Aug</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Sep</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Oct</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+              </View>
+              <View style={styles.tableChecKBoxContainer}>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Nov</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+                <View style={styles.tableCheckBox}>
+                  <Text style={styles.CardText({ fonSize: 20 })}>Dec</Text>
+                  <Checkbox
+                    style={styles.checkBox}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? "#4630EB" : undefined}
+                  />
+                </View>
+              </View>
+            </View>
+            <View style={styles.descriptionContainer}>
+              <View>
+                <Text
+                  style={styles.description({
+                    fontSize: 14,
+                    fontWeight: "700",
+                  })}
+                >
+                  You selected 5 months
+                </Text>
+                <View style={styles.descriptionSubContainer}>
+                  <Text
+                    style={styles.description({
+                      fontSize: 12,
+                      fontWeight: "400",
+                    })}
+                  >
+                    $4.99 1 month x 5 months
+                  </Text>
+                  <Text
+                    style={styles.description({
+                      fontSize: 14,
+                      fontWeight: "700",
+                    })}
+                  >
+                    $24.99
+                  </Text>
+                </View>
+              </View>
+              <View>
+                <View style={styles.descriptionSubContainer}>
+                  <Text
+                    style={styles.description({
+                      fontSize: 14,
+                      fontWeight: "700",
+                    })}
+                  >
+                    Total
+                  </Text>
+                  <Text
+                    style={styles.description({
+                      fontSize: 17,
+                      fontWeight: "700",
+                    })}
+                  >
+                    $24.99
+                  </Text>
+                </View>
+                <Text
+                  style={styles.description({
+                    fontSize: 14,
+                    fontWeight: "400",
+                  })}
+                >
+                  See your total (including taxes)
+                </Text>
+              </View>
+            </View>
             <TouchableOpacity
               style={{
                 backgroundColor: "#005858",
-                paddingVertical: 6,
-                paddingHorizontal: 12,
-                borderRadius: 12,
-                marginTop: 14,
+                width: "69%",
+                height: 50,
+                borderRadius: 10,
+                marginHorizontal: 40,
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
               <Text
@@ -36,6 +212,7 @@ const ChoosePlanCalender = () => {
                   fontSize: 16,
                   marginVertical: 10,
                   color: "#fff",
+                  width: 170,
                 })}
               >
                 Proceed to payment
@@ -59,9 +236,9 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   headerContainer: {
-    flex: 1,
     alignItems: "center",
     paddingTop: 50,
+    paddingBottom: 60
   },
   header: {
     color: "#fff",
@@ -71,7 +248,7 @@ const styles = StyleSheet.create({
   },
   card: {
     backgroundColor: "#fff",
-    height: 450,
+    height: 500,
     width: "84%",
     borderWidth: 4,
     borderColor: "#005858",
@@ -86,12 +263,49 @@ const styles = StyleSheet.create({
     marginVertical: data.marginVertical,
     fontWeight: "700",
     color: data.color,
+    width: data.width,
   }),
   footerText: {
     color: "#fff",
     fontSize: 17,
     fontWeight: "800",
   },
+  table: {
+    marginHorizontal: 27,
+    borderWidth: 2,
+    borderColor: "#005858",
+    padding: 14,
+    height: 220,
+    width: "79%",
+    marginVertical: 13,
+  },
+  tableCheckBox: {
+    width: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    gap: 4,
+  },
+  tableChecKBoxContainer: {
+    flexDirection: "row",
+  },
+  checkBox: {
+    height: 20,
+    width: 20,
+  },
+  descriptionContainer: {
+    paddingHorizontal: 25,
+    marginVertical: 5,
+  },
+  descriptionSubContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  description: (data) => ({
+    fontSize: data.fontSize,
+    fontWeight: data.fontWeight,
+    marginVertical: 3
+  }),
 });
 
 export default ChoosePlanCalender;
